cabf58c1b65b0bc75a0d8b20dc9a44c2a817dabc
Uladzimir Havenchyk <uladzimir_havenchyk@epam.com>
2015-03-24 17:02:25 +0300

    Added server api

--- server.js
+++ server.js
@@ -20,6 +20,99 @@ app.use(errorHandler({
   showStack: true
 }));
 
+app.get('/api', function(req, res){
+	res.send('hello world');
+});
+
+mongoose.connect('mongodb://localhost/library');
+
+var Book = new mongoose.Schema({
+	title: String,
+	author: String,
+	releaseDate: Date
+});
+
+var BookModel = mongoose.model('Book', Book);
+
+app.get('/api/books', function(request, response) {
+	return BookModel.find(function(err, books) {
+		if (!err) {
+			return response.send(books);
+		} else {
+			return console.log(err);
+		}
+	});
+});
+
+app.post('/api/books', function(request, response) {
+	console.log(request.body.title);
+
+	var book = new BookModel({
+		title: request.body.title,
+		author: request.body.author,
+		releaseDate: request.body.releaseDate
+	});
+
+	book.save(function(err) {
+		if (!err) {
+			return console.log('created');
+		} else {
+			return console.log(err);
+		}
+	});
+
+	return response.send(book);
+});
+
+app.get('/api/books/:id', function(request, response) {
+	console.log(request.params.id);
+
+	return BookModel.findById(request.params.id, function(err, book) {
+		if (!err) {
+			return response.send(book);
+		} else {
+			return console.log(err);
+		}
+	})
+});
+
+app.put('/api/books/:id', function(request, response) {
+	console.log('Updating book' + request.body.title);
+
+	return BookModel.findById(request.params.id, function(err, book) {
+		book.title = request.body.title;
+		book.author = request.body.author;
+		book.releaseDate = request.body.releaseDate;
+
+		return book.save(function(err) {
+			if (!err) {
+				console.log('book updated');
+			} else {
+				console.log(err);
+			}
+
+			return response.send(book);
+		})
+	});
+});
+
+app.delete('/api/books/:id', function(request, response) {
+	console.log('Deleting book with id: ' + request.params.id);
+
+	return BookModel.findById(request.params.id, function(err, book) {
+		return book.remove(function(err) {
+			if (!err) {
+				console.log('Book removed');
+
+				return response.send('');
+			} else {
+				console.log(err);
+			}
+		});
+	});
+});
+
+
 var port = 4711;
 
 app.listen(port, function () {
